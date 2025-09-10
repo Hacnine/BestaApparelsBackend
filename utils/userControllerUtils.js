d4f1a7e2da7f81228d07c59a91fc9839feeb769d
Hasanain <hasanaintushar@gmail.com>
2025-09-10 12:40:28 -0700

    Update userControllerUtils.js

--- utils/userControllerUtils.js
+++ utils/userControllerUtils.js
@@ -2,11 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const checkAdmin = async (userId) => {
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-  });
+export const checkAdmin = async (user) => {
   if (!user || user.role !== 'ADMIN') {
     throw new Error('Unauthorized: Only admins can perform this action');
+  }
+  else{
+    return true;
   }
 };
