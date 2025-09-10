1b19c626ede1a2ab5f68b24464793b97eecb71f7
Hasanain <hasanaintushar@gmail.com>
2025-09-10 12:40:48 -0700

    Update apiIndex.js

--- routes/apiIndex.js
+++ routes/apiIndex.js
@@ -1,15 +1,16 @@
-// routes/index.js
+
 import { Router } from "express";
 
 import userRouter from "./userRoute.js";
 import tnaRouter from "./tnaRoute.js";
 import auditRouter from "./auditRoute.js";
 import dashboardRouter from "./dashboardRoute.js";
+import employeeRouter from "./employeeRoute.js";
 
 const apiRoute = Router();
 
-// Mount all API routes
 apiRoute.use("/user", userRouter);
+apiRoute.use("/employee", employeeRouter);
 apiRoute.use("/tnas", tnaRouter);
 apiRoute.use("/audit-logs", auditRouter);
 apiRoute.use("/dashboard", dashboardRouter);
