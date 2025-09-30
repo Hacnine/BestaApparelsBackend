b4896da4efedd05eb7aea74fc89ee1dc0a95c995
Hasanain <hasanaintushar@gmail.com>
2025-09-30 19:15:07 +0600

    buyer controller and functionality updated

--- routes/merchandiserRoute.js
+++ routes/merchandiserRoute.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import { requireAuth } from '../middlewares/authMiddleware.js';
-import { createBuyer, createDepartment, getBuyers, getDepartments, getMerchandisers } from '../controllers/merchandiserController.js';
+import { createDepartment, getDepartments, getMerchandisers } from '../controllers/merchandiserController.js';
 import { createTna } from '../controllers/tnaController.js';
 
 const merchandiserRoute = express.Router();
 
 merchandiserRoute.use(requireAuth);
-merchandiserRoute.post('/create-buyer', createBuyer);
 merchandiserRoute.post('/create-department', createDepartment);
 merchandiserRoute.post('/create-tna', createTna);
 merchandiserRoute.get('/merchandisers', getMerchandisers);
-merchandiserRoute.get('/buyers', getBuyers);
-merchandiserRoute.post('/departments', getDepartments);
+merchandiserRoute.get('/departments', getDepartments);
 
 
 
